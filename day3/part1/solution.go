ff0e05df2fd3a9ba302e33a298baf68fee890163
Niraj-Fonseka <fonseka.live@gmail.com>
2020-12-03 21:33:46 -0600

    day3 cleanup

--- day3/part1/solution.go
+++ day3/part1/solution.go
@@ -19,15 +19,6 @@ func main() {
 		}
 	}
 
-	fmt.Println(grid)
-	// for row := 0; row < len(grid[0]); row++ {
-
-	// 	for col := 0; col < len(grid); col++ {
-	// 		fmt.Printf("%c ", grid[row][col])
-	// 	}
-	// 	fmt.Println()
-	// }
-
 	stepRow := 0
 	stepCol := 0
 	trees := 0
@@ -35,48 +26,18 @@ func main() {
 		stepCol += 3
 		stepRow++
 
-		// fmt.Println(len(grid[0])) -> 11
 		if stepCol > len(grid[0])-1 { //overstep x
-			//fmt.Println("Fixing....")
-			//fmt.Printf(" %d - %d = %d \n", stepCol, (len(grid[0]) - 2), (stepCol - (len(grid[0]) - 2)))
 			stepCol = (stepCol - (len(grid[0])))
 		}
-
-		//fmt.Printf("Col : %d , Row : %d  , Char : %c \n", stepCol, stepRow, grid[stepRow][stepCol])
 
 		if stepRow > len(grid)-1 {
 			break
 		}
-		//fmt.Printf("Checking X : %d , Y : %d  --> %c \n", stepCol, stepRow, grid[stepRow][stepCol])
 		if grid[stepRow][stepCol] == 35 {
-			fmt.Printf("Found a tree at col : %d , row : %d \n", stepCol, stepRow)
 			trees++
 		}
 
-		//time.Sleep(1 * time.Second)
 	}
-	fmt.Println("Total trees --> ", trees)
-
-	// y := 0
-	// x := 0
-	// trees := 0
-
-	// for {
-
-	// 	if y < len(grid) {
-	// 		if grid[y][x%len(grid[0])] == '#' {
-
-	// 			//fmt.Printf("Found a tree at col : %d , row : %d , char : %c \n", col%len(grid[0]), row, grid[row][col%len(grid[0])])
-	// 			trees++
-	// 		}
-
-	// 		y += 1
-	// 		x += 3
-	// 	} else {
-	// 		break
-	// 	}
-
-	// }
 
 	fmt.Println("Total Trees :-> ", trees)
 
